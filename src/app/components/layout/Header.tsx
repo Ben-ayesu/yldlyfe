f42cc4a0088b2fc71f420e1490c01ea40af9d00f
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-11-22 08:54:23 -0800

    add margin top to products

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
         <motion.header
             style={{ backgroundColor: headerBg }}
             className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-                isScrolled ? "py-4" : "py-8"
+                isScrolled ? "py-4" : "py-6"
             }`}
         >
             <div className="container mx-auto px-4 flex justify-between items-center">
